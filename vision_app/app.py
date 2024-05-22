4c0bf5f9e7843c4eaa63bab116868d3157ba73bd
QuangquyNguyenvo <128112180+QuangquyNguyenvo@users.noreply.github.com>
2024-05-22 10:56:31 +0700

    Class

--- vision_app/app.py
+++ vision_app/app.py
@@ -18,6 +18,22 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key.json'
 # Tạo thư mục upload nếu chưa tồn tại
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
+# tạo list py
+def determine_waste_type(labels):
+    organic_labels = ['food', 'fruit', 'vegetable', 'plant', 'organic', 'compost', 'biodegradable', 'leaf', 'wood', 'flower', 'seed', 'grain', 'bread', 'meat', 'dairy', 'egg','human','animal']
+    inorganic_labels = ['plastic', 'glass', 'metal', 'paper', 'cardboard', 'aluminum', 'steel', 'tin', 'ceramic', 'concrete', 'stone', 'rubber', 'leather','rubber band']
+    electronic_labels = ['electronic', 'computer', 'phone', 'circuit', 'laptop', 'monitor', 'keyboard', 'printer', 'cable', 'battery', 'chip', 'motherboard', 'speaker', 'headphone', 'camera', 'television', 'remote']
+
+    for label in labels:
+        description = label.description.lower()
+        if any(word in description for word in organic_labels):
+            return 'Hữu cơ'
+        elif any(word in description for word in inorganic_labels):
+            return 'Vô cơ'
+        elif any(word in description for word in electronic_labels):
+            return 'Linh kiện điện tử'
+
+    return 'Khác'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,6 +50,8 @@ def index():
             labels, modified_image_path = detect_labels_and_objects(filepath)
             session['labels'] = [(label.description, label.score) for label in labels]
             session['image_url'] = modified_image_path
+            waste_type = determine_waste_type(labels)
+            session['waste_type'] = waste_type
             return redirect(url_for('result'))
     return render_template('index.html')
 
@@ -41,7 +59,8 @@ def index():
 def result():
     labels = session.get('labels', [])
     image_url = session.get('image_url', '')
-    return render_template('result.html', labels=labels, image_url=image_url)
+    waste_type = session.get('waste_type', 'Khác')
+    return render_template('result.html', labels=labels, image_url=image_url, waste_type=waste_type)
 
 def detect_labels_and_objects(path):
     client = vision.ImageAnnotatorClient()
